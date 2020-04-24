e7d4d5124ec88c1313703862a119a6cd71f64c6f
Steven <anguelr12@hotmail.com>
2020-04-24 16:49:12 -0500

    import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,13 @@
+/* Populate tables */
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Angel', 'Rodriguez', 'correo@email.com', '2019-03-01');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Steven', 'Rodriguez', 'correo1@email.com', '2019-03-02');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Emanuel', 'Rodriguez', 'correo2@email.com', '2019-03-03');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Jhon', 'Lopez', 'correo3@email.com', '2019-03-04');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Angelo', 'Gutierrez', 'correo4@email.com', '2019-03-05');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Nombre1', 'Apellido1', 'correo5@email.com', '2019-03-06');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Nombre2', 'Apellido2', 'correo6@email.com', '2019-03-07');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Nombre3', 'Apellido3', 'correo7@email.com', '2019-03-08');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Nombre4', 'Apellido4', 'correo8@email.com', '2019-03-09');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Nombre5', 'Apellido5', 'correo9@email.com', '2019-03-10');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Nombre6', 'Apellido6', 'correo10@email.com', '2019-03-11');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES('Nombre7', 'Apellido7', 'correo11@email.com', '2019-03-12');
